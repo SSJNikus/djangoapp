b80b8fe2d69a680a9b14688191f0ea50b0b0d398
SSJNikus <137408920+SSJNikus@users.noreply.github.com>
2023-06-22 18:01:38 +0200

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,24 +1,67 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+class Gear(models.Model):
+    name = models.CharField(max_length=128,
+                            unique=True,
+                            verbose_name="Nazwa")
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    class Meta:
+        ordering = ["name"]
+    
     def __str__(self):
-        return self.choice_text
+        return f"{self.name}"
+    
+class Fuel(models.Model):
+    namen = models.CharField(max_length=128,
+                            unique=True,
+                            verbose_name="Nazwa")
+
+    class Meta:
+        ordering = ["namen"]
+    
+    def __str__(self):
+        return f"{self.namen}"
+
+
+class Cars(models.Model):
+    brand = models.CharField(max_length=128,
+                                  null=False,
+                                  default="",
+                                  verbose_name="Marka")
+    model = models.CharField(max_length=256,
+                                 null=False,
+                                 default="",
+                                 verbose_name="Model")
+    made_date = models.CharField(max_length=4,
+                                 null=False,
+                                 default="",
+                                 verbose_name="Rok Produkcji")
+    price = models.DecimalField(max_digits=12,
+                                 decimal_places=2,
+                                 default=0,
+                                 verbose_name="Cena")
+    horse = models.IntegerField(verbose_name="Moc")
+    description = models.TextField(default="",
+                                   blank=True,
+                                   verbose_name="Opis")
+    image = models.ImageField(upload_to="cars/",
+                              null=True,
+                              blank=True,
+                              verbose_name="Zdjecie")
+    gear = models.ForeignKey(Gear,
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name="Skrzynia Biegow")
+    fuel = models.ForeignKey(Fuel,
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name="Paliwo")
+    
+    
+    class Meta:
+        ordering = ["model"]
+    
+    def __str__(self):
+        return f"{self.brand} {self.model}"
